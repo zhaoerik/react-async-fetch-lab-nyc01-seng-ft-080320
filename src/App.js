28041ad7f38aae3d47c275363596eaeaa05522ee
Erik Zhao <erikzhao98@gmail.com>
2020-10-13 13:41:53 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
 // create your App component here
+
+import React from 'react';
+
+class App extends React.Component {
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+    }
+}
+
+export default App;
