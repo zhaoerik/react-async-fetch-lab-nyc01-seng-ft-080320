fa791b3c60ff20b45273286e9102be691a4bf69b
Erik Zhao <erikzhao98@gmail.com>
2020-10-13 13:48:26 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 // create your App component here
-
 import React from 'react';
 
 class App extends React.Component {
 
+    state = {
+        people: []
+    }
+
     componentDidMount() {
         fetch("http://api.open-notify.org/astros.json")
+        .then(res => res.json())
+        .then(data => this.setState({ people: data }))
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.people.map(el => <h1>key={el.id} name={el.name}</h1>)}
+            </div>
+        )
     }
 }
 
